b32a8c5f2389b6055fa892e87ffe26ecb5f1216c
Kingsquare BV <source@kingsquare.nl>
2020-02-09 23:54:08 +0100

    Got DXF in proper window

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Old from "./Old";
 import NavBar from "./components/NavBar";
 
 import {
@@ -11,36 +10,16 @@ import {
   MosaicZeroState
 } from "react-mosaic-component";
 
-import { CloseAdditionalControlsButton } from "./CloseAdditionalControlsButton";
-
 import "@blueprintjs/core/src/blueprint.scss";
 // import "@blueprintjs/icons/src/blueprint-icons.scss";
 import "react-mosaic-component/react-mosaic-component.css";
-import DxfProvider from "./provider/Dxf";
-// import "../styles/index.less";
-// import "./example.less";
-
-// tslint:disable no-console
-
-// tslint:disable-next-line no-var-requires
+import DxfProvider, { DxfContext } from "./provider/Dxf";
+import { MosaicBranch } from "react-mosaic-component/src/types";
 
 let windowCount = 3;
 
-export const THEMES = {
-  Blueprint: "mosaic-blueprint-theme"
-};
-
-export type Theme = keyof typeof THEMES;
-
-const additionalControls = React.Children.toArray([
-  <CloseAdditionalControlsButton />
-]);
-
-const EMPTY_ARRAY: any[] = [];
-
 export interface ExampleAppState {
-  currentNode: MosaicNode<number> | null;
-  currentTheme: Theme;
+  currentNode: MosaicNode<string> | null;
 }
 
 export default class ExampleApp extends React.PureComponent<
@@ -50,15 +29,74 @@ export default class ExampleApp extends React.PureComponent<
   state: ExampleAppState = {
     currentNode: {
       direction: "row",
-      first: 1,
+      first: {
+        direction: "row",
+        first: "dxfData",
+        second: "instructions"
+      },
       second: {
         direction: "column",
-        first: 2,
-        second: 3
+        first: "test1",
+        second: "test2"
       },
       splitPercentage: 80
-    },
-    currentTheme: "Blueprint"
+    }
+  };
+
+  private renderTile = (count: string, path: MosaicBranch[]) => {
+    switch (count) {
+      case "dxfData":
+        return (
+          <MosaicWindow
+            title={`DXF data`}
+            createNode={this.createNode}
+            path={path}
+            onDragStart={() => console.log("MosaicWindow.onDragStart")}
+            onDragEnd={type => console.log("MosaicWindow.onDragEnd", type)}
+          >
+            <DxfContext.Consumer key={count}>
+              {context => (
+                <pre style={{ maxHeight: "100%", overflow: "auto" }}>
+                  {JSON.stringify(context.dxfData, null, "\t")}
+                </pre>
+              )}
+            </DxfContext.Consumer>
+          </MosaicWindow>
+        );
+        break;
+      case "instructions":
+        return (
+          <MosaicWindow
+            title={`Window ${count}`}
+            createNode={this.createNode}
+            path={path}
+            onDragStart={() => console.log("MosaicWindow.onDragStart")}
+            onDragEnd={type => console.log("MosaicWindow.onDragEnd", type)}
+          >
+            <div className="example-window">
+              <h1>{`Window ${count}`}</h1>
+            </div>
+            )}
+          </MosaicWindow>
+        );
+        break;
+
+      default:
+        return (
+          <MosaicWindow
+            title={`Window ${count}`}
+            createNode={this.createNode}
+            path={path}
+            onDragStart={() => console.log("MosaicWindow.onDragStart")}
+            onDragEnd={type => console.log("MosaicWindow.onDragEnd", type)}
+          >
+            <div className="example-window">
+              <h1>{`Window ${count}`}</h1>
+            </div>
+            )}
+          </MosaicWindow>
+        );
+    }
   };
 
   render() {
@@ -66,55 +104,24 @@ export default class ExampleApp extends React.PureComponent<
       <DxfProvider>
         <div className="react-mosaic-example-app">
           <NavBar />
-          <Mosaic<number>
-            renderTile={(count, path) => {
-              console.log(count, path);
-              return (
-                <MosaicWindow<number>
-                  additionalControls={
-                    count === 3 ? additionalControls : EMPTY_ARRAY
-                  }
-                  title={`Window ${count}`}
-                  createNode={this.createNode}
-                  path={path}
-                  onDragStart={() => console.log("MosaicWindow.onDragStart")}
-                  onDragEnd={type =>
-                    console.log("MosaicWindow.onDragEnd", type)
-                  }
-                  renderToolbar={
-                    count === 2
-                      ? () => (
-                          <div className="toolbar-example">Custom Toolbar</div>
-                        )
-                      : null
-                  }
-                >
-                  {count === 1 ? (
-                    <Old />
-                  ) : (
-                    <div className="example-window">
-                      <h1>{`Window ${count}`}</h1>
-                    </div>
-                  )}
-                </MosaicWindow>
-              );
-            }}
+          <Mosaic
+            renderTile={this.renderTile}
             zeroStateView={<MosaicZeroState createNode={this.createNode} />}
             value={this.state.currentNode}
             onChange={this.onChange}
             onRelease={this.onRelease}
-            className={THEMES[this.state.currentTheme]}
+            className={"mosaic-blueprint-theme"}
           />
         </div>
       </DxfProvider>
     );
   }
 
-  private onChange = (currentNode: MosaicNode<number> | null) => {
+  private onChange = (currentNode: MosaicNode<string> | null) => {
     this.setState({ currentNode });
   };
 
-  private onRelease = (currentNode: MosaicNode<number> | null) => {
+  private onRelease = (currentNode: MosaicNode<string> | null) => {
     console.log("Mosaic.onRelease():", currentNode);
   };
 
@@ -126,48 +133,5 @@ export default class ExampleApp extends React.PureComponent<
     this.setState({
       currentNode: createBalancedTreeFromLeaves(leaves)
     });
-  };
-
-  private addToTopRight = () => {
-    let { currentNode } = this.state;
-    if (currentNode) {
-      // const path = getPathToCorner(currentNode, Corner.TOP_RIGHT);
-      // const parent = getNodeAtPath(
-      //   currentNode,
-      //   dropRight(path)
-      // ) as MosaicParent<number>;
-      // const destination = getNodeAtPath(currentNode, path) as MosaicNode<
-      //   number
-      // >;
-      // const direction: MosaicDirection = parent
-      //   ? getOtherDirection(parent.direction)
-      //   : "row";
-      // let first: MosaicNode<number>;
-      // let second: MosaicNode<number>;
-      // if (direction === "row") {
-      //   first = destination;
-      //   second = ++windowCount;
-      // } else {
-      //   first = ++windowCount;
-      //   second = destination;
-      // }
-      //
-      // currentNode = updateTree(currentNode, [
-      //   {
-      //     path,
-      //     spec: {
-      //       $set: {
-      //         direction,
-      //         first,
-      //         second
-      //       }
-      //     }
-      //   }
-      // ]);
-    } else {
-      currentNode = ++windowCount;
-    }
-
-    this.setState({ currentNode });
   };
 }
