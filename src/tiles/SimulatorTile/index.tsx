502e4e72ac834252dadaf73770fa6954ed5a8d4a
Kingsquare BV <source@kingsquare.nl>
2020-02-10 21:51:50 +0100

    Got grid and helper lines

--- src/tiles/SimulatorTile/index.tsx
+++ src/tiles/SimulatorTile/index.tsx
@@ -4,6 +4,8 @@ import { MosaicWindow } from "react-mosaic-component";
 import React from "react";
 
 import * as THREE from "three";
+import { Material } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // const instructionVisualizer = new InstructionVisualizer();
 
@@ -14,20 +16,45 @@ export default class SimulatorTile extends React.Component<ITileProps> {
     if (!this.mount) {
       throw new Error("SimulatorTile missing mount point?");
     }
+    const width = (this.mount.parentNode as HTMLDivElement).clientWidth;
+    const height = (this.mount.parentNode as HTMLDivElement).clientHeight;
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
+    renderer.setClearColor(0xeeeeee, 1);
+    renderer.setSize(width, height);
     this.mount.appendChild(renderer.domElement);
+    new OrbitControls(camera, this.mount);
+    // the grid
+
+    // grid
+    // will get mm or inches for grid
+    const widthHeightOfGrid = 200; // 200 mm grid should be reasonable
+    const subSectionsOfGrid = 10; // 10mm (1 cm) is good for mm work
+
+    // see if user wants to size up grid. default is size 1
+    // so this won't modify size based on default
+
+    // draw grid
+    const grid = new THREE.GridHelper(
+      widthHeightOfGrid,
+      subSectionsOfGrid,
+      0x0000ff,
+      0x808080
+    );
+    grid.position.y = 0;
+    grid.position.x = 0;
+    grid.position.z = 0;
+    grid.rotation.x = (90 * Math.PI) / 180;
+    let material = grid.material as Material;
+    material.opacity = 0.15;
+    material.transparent = true;
+    grid.receiveShadow = false;
+    // scene.position.x = -150;
+    scene.add(grid);
+
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
@@ -37,6 +64,8 @@ export default class SimulatorTile extends React.Component<ITileProps> {
       cube.rotation.y += 0.01;
       renderer.render(scene, camera);
     };
+    const axisHelper = new THREE.AxesHelper(5);
+    scene.add(axisHelper);
     animate();
   }
   render() {
