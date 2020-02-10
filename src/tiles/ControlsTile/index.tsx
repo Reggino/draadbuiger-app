16e45b8be70d32960efc5023167287df019be048
Kingsquare BV <source@kingsquare.nl>
2020-02-10 22:44:13 +0100

    Got manual control buttons

--- src/tiles/ControlsTile/index.tsx
+++ src/tiles/ControlsTile/index.tsx
@@ -1,9 +1,46 @@
 import { ITileProps } from "../index";
 import { MosaicWindow } from "react-mosaic-component";
 import React from "react";
+import { Button } from "@blueprintjs/core";
+
+import "./index.scss";
 
 export default ({ path }: ITileProps) => (
   <MosaicWindow title={`Manuele besturing`} path={path}>
-    hier komen de knoppen
+    <table className="tiles__controls-tile__table">
+      <tr>
+        <td>
+          <Button
+            large={true}
+            icon="refresh"
+            className="tiles__controls-tile__rotate-left"
+            style={{ transform: "rotate(180deg)" }}
+          />
+        </td>
+        <td>
+          <Button icon="arrow-up" large={true} />
+        </td>
+        <td>
+          <Button icon="refresh"  large={true} />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <Button icon="arrow-left" large={true} />
+        </td>
+        <td>
+          <Button icon="home" large={true} />
+        </td>
+        <td>
+          <Button icon="arrow-right" large={true} />
+        </td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>
+          <Button icon="arrow-down" large={true} />
+        </td>
+      </tr>
+    </table>
   </MosaicWindow>
 );
